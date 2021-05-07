467bccb5e5ec71add6db5a79b4f9ffd7cf6a679e
Raphael Becker <ge38xed@tum.de>
2021-05-08 00:02:07 +0200

    calculation bug fixes

--- calculator.py
+++ calculator.py
@@ -3,10 +3,10 @@ def calc_short_trade(availalbe_balance, initial_bet, start_price, leverage):
     return_string += 'DISCLAIMER: ALWAYS DOUBLE CHECK THE CALCULATIONS \n - NO GUARANTEE OF ACCURACY!\n \nSHORT trade ' \
                     'calculation:\n '
     i = 0
-    percent = leverage / 100
+    percent = 1/leverage
     correction_account = availalbe_balance - initial_bet
     bet = initial_bet
-    liq_price = start_price + (percent - percent * 0.025) * start_price
+    liq_price = start_price + (percent - percent * 0.026) * start_price
     return_string += str(f'\nAvailable Balance: {correction_account} ')
     return_string += str(f' Initial trade: {initial_bet} ')
     return_string += str(f'\nEntry price: {start_price:.2f} ')
@@ -21,14 +21,14 @@ def calc_short_trade(availalbe_balance, initial_bet, start_price, leverage):
         bet = bet * 2
         i += 1
         start_price = start_price + 0.5 * (liq_price - start_price)
-        liq_price = start_price + (percent - percent * 0.025) * start_price
+        liq_price = start_price + (percent - percent * 0.026) * start_price
         return_string += str(f'\nNew entry price: {start_price:.2f} ')
         return_string += str(f'Liquidation price: {liq_price:.2f} ')
         return_string += '\n'
     return_string += '\n**********************************\n'
     return_string += str(f'Corrections possible: {i} ')
     start_price = start_price + (availalbe_balance / (trade_vol + availalbe_balance)) * (liq_price - start_price)
-    liq_price = start_price + (percent - percent * 0.025) * start_price
+    liq_price = start_price + (percent - percent * 0.026) * start_price
     return_string += str(f'\nFinal liquidation price: {liq_price:.2f} \n \n \n')
     return return_string
 
@@ -38,10 +38,10 @@ def calc_long_trade(availalbe_balance, initial_bet, start_price, leverage):
     return_string += 'DISCLAIMER: ALWAYS DOUBLE CHECK THE CALCULATIONS \n - NO GUARANTEE OF ACCURACY!\n \nLONG trade ' \
                     'calculation:\n '
     i = 0
-    percent = leverage / 100
+    percent = 1/leverage
     correction_account = availalbe_balance - initial_bet
     bet = initial_bet
-    liq_price = start_price - (percent - percent * 0.025) * start_price  # + long!
+    liq_price = start_price - (percent - percent * 0.026) * start_price  # + long!
     return_string += str(f'\nAvailable Balance: {correction_account} ')
     return_string += str(f' Initial trade: {initial_bet} ')
     return_string += str(f'\nEntry price: {start_price:.2f} ')
@@ -56,14 +56,14 @@ def calc_long_trade(availalbe_balance, initial_bet, start_price, leverage):
         bet = bet * 2
         i += 1
         start_price = start_price - 0.5 * (start_price - liq_price)  # switched +- long!
-        liq_price = start_price - (percent - percent * 0.025) * start_price  # + long!
+        liq_price = start_price - (percent - percent * 0.026) * start_price  # + long!
         return_string += str(f'\nNew entry price: {start_price:.2f} ')
         return_string += str(f'Liquidation price: {liq_price:.2f} ')
         return_string += '\n'
     return_string += '\n**********************************\n'
     return_string += str(f'Corrections possible: {i} ')
-    start_price = start_price + (availalbe_balance / (trade_vol + availalbe_balance)) * (liq_price - start_price)
-    liq_price = start_price + (percent - percent * 0.025) * start_price
+    start_price = start_price - (correction_account / (trade_vol + correction_account)) * (start_price - liq_price) # switched liq_price and start_price
+    liq_price = start_price - (percent - percent * 0.026) * start_price
     return_string += str(f'\nFinal liquidation price: {liq_price:.2f} \n \n \n')
     return return_string
 
